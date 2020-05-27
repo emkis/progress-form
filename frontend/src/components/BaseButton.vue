bce3899a5cdf706c2e789005b6dfc11b5a092565
Emkis <nicolasemkis@gmail.com>
2020-05-27 04:45:42 -0300

    basic structure of components added

---
+++ frontend/src/components/BaseButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="base__button">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+@import '@/styles/constants/index.scss';
+</style>
--- frontend/src/components/BaseSelectInput.vue
+++ frontend/src/components/BaseSelectInput.vue
@@ -1,9 +1,65 @@
 <template>
-  <div></div>
+  <div class="base-select-input">
+    <label class="base-select-input__label" @click="focusInput">
+      {{ label }}
+    </label>
+
+    <select
+      ref="input"
+      class="base-select-input__action"
+      @input="updateValue"
+      v-bind="$attrs"
+      :value="value"
+      :aria-label="label"
+    >
+      <option
+        v-for="(option, index) in options"
+        :key="option"
+        :value="index"
+        :selected="option === value"
+      >
+        {{ option }}
+      </option>
+    </select>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+    value: [String, Number],
+  },
+  methods: {
+    updateValue({ target }) {
+      this.$emit('input', target.value)
+    },
+    focusInput() {
+      this.$refs.input.focus()
+    },
+  },
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@/styles/constants/index.scss';
+
+.base-select-input {
+  & + & {
+    margin-top: rem(18px);
+  }
+
+  &__label {
+  }
+  &__action {
+  }
+}
+</style>
