cbb00ce1134e6860a924b662cefbe7e4d497ad3f
Emkis <nicolasemkis@gmail.com>
2020-06-02 03:11:52 -0300

    add resize event handler to mixin

--- frontend/src/mixins/fullViewportHeightMixin.js
+++ frontend/src/mixins/fullViewportHeightMixin.js
@@ -1,7 +1,11 @@
 const fullViewportHeightMixin = {
   beforeCreate() {
-    const viewportHeight = window.innerHeight * 0.01
-    document.documentElement.style.setProperty('--vh', `${viewportHeight}px`)
+    function handleResize() {
+      const viewportHeight = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${viewportHeight}px`)
+    }
+
+    window.addEventListener('resize', handleResize)
   },
 }
 
