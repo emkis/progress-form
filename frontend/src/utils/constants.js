51b01b258182888c0dd2c0b60c4b98b236fdb9ce
Emkis <nicolasemkis@gmail.com>
2020-06-02 03:12:20 -0300

    structure of constants changed

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,99 +1,258 @@
-export const categories = [
-  {
-    name: 'realistic',
-    questions: [
-      'I am very authentic and say what I think.',
-      `I'm shy, I think a lot before I expose myself.`,
-      'Deep intellectual issues do not interest me.',
-      'I am persistent and do not give up until I reach my goals.',
-      'People can count on me. I am quite stable in my actions.',
-      'I enjoy working with materials, hands, objects and instruments.',
-      'I can handle physical or intellectual activity for a long time.',
-      'I give a lot of value for money.',
-      'I know how to use electronic instruments and devices.',
-      'I accept the rules, limits, orders and the context in which I live.',
-    ],
-  },
-  {
-    name: 'investigative',
-    questions: [
-      `I like to observe, analyze and understand the way things, nature or people work.`,
-      `I'm not very impulsive, I think before I act.`,
-      `Areas like math or science attract me. I like to study a subject in depth.`,
-      `I find it easy to learn.`,
-      `I am rational in making a decision or in deciding the best way forward.`,
-      `I am very organized and detail oriented.`,
-      'I am able to give precise answers about dates, values ​​and describe a certain situation in the past.',
-      `I have a great critical sense. Things can always be improved.`,
-      `I like to make my own decisions, even if it is different from the thinking of the majority.`,
-      `I have good logical or mathematical skills.`,
-    ],
-  },
-  {
-    name: 'entrepreneur',
-    questions: [
-      `I am firm and confident in my decisions and I don't need guesses.`,
-      `I can often convince people of a certain point of view.`,
-      `I am a motivated, self-motivated, optimistic person.`,
-      `I talk too much. I don't easily give up on an argument.`,
-      `I place a high value on material goods. I like to buy and sometimes even demonstrate to others my achievements, my material possessions, my wisdom and my talents.`,
-      `I make decisions quickly. I act without much thought. I get to be impulsive.`,
-      `I like being with people, especially to show them the way I think.`,
-      `I love adventures, taking risks and challenges.`,
-      `I am always looking for new opportunities, new paths to follow.`,
-      `I'd rather be a business owner than work for someone else.`,
-    ],
-  },
-  {
-    name: 'conventional',
-    questions: [
-      `I accept the rules and the context in which I live. I like clear boundaries.`,
-      `I am an introvert, I guard myself and do not expose myself much.`,
-      `I take care of my things and I like to keep them.`,
-      `I am very calm and have a good emotional balance.`,
-      `I prefer to have an action plan, a defined work routine.`,
-      `I am a stable, reliable type of person suited to reality.`,
-      `I respect ethics, I keep secrets, I comply with my agreements.`,
-      `I am very organized and detailed in things. Effectiveness is very important to me.`,
-      `I go all the way in my goals. I don't give up until I reach them.`,
-      `I like to satisfy others in relation to what they ask of me.`,
-    ],
-  },
-  {
-    name: 'artistic',
-    questions: [
-      `It is difficult for me to accept rules, limits and something imposed on me.`,
-      `It is easy for me to live in a disorganized environment and do things without logic.`,
-      `I'm very sensitive. I use emotion more in my decisions.`,
-      `I am an idealistic person. I believe we can build a better world.`,
-      `I like to create, invent, idealize and fantasize.`,
-      `I like to make an impact, provoking, entertaining, seducing, enchanting people.`,
-      `I like to discover new possibilities for things and situations in life.`,
-      `I often don't think long before making a decision, nor do I measure the consequences.`,
-      `I like to express myself, to represent myself or to show myself in public.`,
-      `I am responsible for my attitudes and decisions.`,
-    ],
-  },
-  {
-    name: 'social',
-    questions: [
-      `I like to lead, organize or mobilize a group around a cause or action.`,
-      `I like to help and collaborate with others. I am loyal and I like to do good.`,
-      `I use emotion and sensitivity to make decisions.`,
-      `I am generally a pleasant and easy going person.`,
-      `I am an idealist and I want more out of life.`,
-      `Ethical, justice or common welfare issues are important to me.`,
-      `I'm responsible. You can count on me because I always live up to expectations.`,
-      `I tend to dedicate myself to people, or causes, "wearing the shirt" of a group or club that I believe in.`,
-      `In general, I relate well with others. I find it easy to talk.`,
-      `I like to help, teach, take care, somehow I want to be important to others.`,
-    ],
-  },
+export const answerAlternatives = [
+  { option: 'a', answer: 'Has nothing to do with me', value: 0 },
+  { option: 'b', answer: 'Has a little to do with me', value: 1 },
+  { option: 'c', answer: 'Has a lot to do with me', value: 2 },
+  { option: 'd', answer: `That's how I am!`, value: 3 },
 ]
 
-export const answerOptions = [
-  'Has nothing to do with me',
-  'Has a little to do with me',
-  'Has a lot to do with me',
-  `That's how I am!`,
+export const questions = [
+  { type: 'realistic', question: 'I am very authentic and say what I think.' },
+  {
+    type: 'realistic',
+    question: `I'm shy, I think a lot before I expose myself.`,
+  },
+  {
+    type: 'realistic',
+    question: 'Deep intellectual issues do not interest me.',
+  },
+  {
+    type: 'realistic',
+    question: 'I am persistent and do not give up until I reach my goals.',
+  },
+  {
+    type: 'realistic',
+    question: 'People can count on me. I am quite stable in my actions.',
+  },
+  {
+    type: 'realistic',
+    question: 'I enjoy working with materials, hands, objects and instruments.',
+  },
+  {
+    type: 'realistic',
+    question: 'I can handle physical or intellectual activity for a long time.',
+  },
+  { type: 'realistic', question: 'I give a lot of value for money.' },
+  {
+    type: 'realistic',
+    question: 'I know how to use electronic instruments and devices.',
+  },
+  {
+    type: 'realistic',
+    question:
+      'I accept the rules, limits, orders and the context in which I live.',
+  },
+  {
+    type: 'investigative',
+    question:
+      'I like to observe, analyze and understand the way things, nature or people work.',
+  },
+  {
+    type: 'investigative',
+    question: `I'm not very impulsive, I think before I act.`,
+  },
+  {
+    type: 'investigative',
+    question:
+      'Areas like math or science attract me. I like to study a subject in depth.',
+  },
+  { type: 'investigative', question: 'I find it easy to learn.' },
+  {
+    type: 'investigative',
+    question:
+      'I am rational in making a decision or in deciding the best way forward.',
+  },
+  {
+    type: 'investigative',
+    question: 'I am very organized and detail oriented.',
+  },
+  {
+    type: 'investigative',
+    question:
+      'I am able to give precise answers about dates, values ​​and describe a certain situation in the past.',
+  },
+  {
+    type: 'investigative',
+    question: 'I have a great critical sense. Things can always be improved.',
+  },
+  {
+    type: 'investigative',
+    question:
+      'I like to make my own decisions, even if it is different from the thinking of the majority.',
+  },
+  {
+    type: 'investigative',
+    question: 'I have good logical or mathematical skills.',
+  },
+  {
+    type: 'entrepreneur',
+    question: `I am firm and confident in my decisions and I don't need guesses.`,
+  },
+  {
+    type: 'entrepreneur',
+    question: 'I can often convince people of a certain point of view.',
+  },
+  {
+    type: 'entrepreneur',
+    question: 'I am a motivated, self-motivated, optimistic person.',
+  },
+  {
+    type: 'entrepreneur',
+    question: `I talk too much. I don't easily give up on an argument.`,
+  },
+  {
+    type: 'entrepreneur',
+    question:
+      'I place a high value on material goods. I like to buy and sometimes even demonstrate to others my achievements, my material possessions, my wisdom and my talents.',
+  },
+  {
+    type: 'entrepreneur',
+    question:
+      'I make decisions quickly. I act without much thought. I get to be impulsive.',
+  },
+  {
+    type: 'entrepreneur',
+    question:
+      'I like being with people, especially to show them the way I think.',
+  },
+  {
+    type: 'entrepreneur',
+    question: 'I love adventures, taking risks and challenges.',
+  },
+  {
+    type: 'entrepreneur',
+    question: 'I am always looking for new opportunities, new paths to follow.',
+  },
+  {
+    type: 'entrepreneur',
+    question: `I'd rather be a business owner than work for someone else.`,
+  },
+  {
+    type: 'conventional',
+    question:
+      'I accept the rules and the context in which I live. I like clear boundaries.',
+  },
+  {
+    type: 'conventional',
+    question:
+      'I am an introvert, I guard myself and do not expose myself much.',
+  },
+  {
+    type: 'conventional',
+    question: 'I take care of my things and I like to keep them.',
+  },
+  {
+    type: 'conventional',
+    question: 'I am very calm and have a good emotional balance.',
+  },
+  {
+    type: 'conventional',
+    question: 'I prefer to have an action plan, a defined work routine.',
+  },
+  {
+    type: 'conventional',
+    question: 'I am a stable, reliable type of person suited to reality.',
+  },
+  {
+    type: 'conventional',
+    question: 'I respect ethics, I keep secrets, I comply with my agreements.',
+  },
+  {
+    type: 'conventional',
+    question:
+      'I am very organized and detailed in things. Effectiveness is very important to me.',
+  },
+  {
+    type: 'conventional',
+    question: `I go all the way in my goals. I don't give up until I reach them.`,
+  },
+  {
+    type: 'conventional',
+    question: 'I like to satisfy others in relation to what they ask of me.',
+  },
+  {
+    type: 'artistic',
+    question:
+      'It is difficult for me to accept rules, limits and something imposed on me.',
+  },
+  {
+    type: 'artistic',
+    question:
+      'It is easy for me to live in a disorganized environment and do things without logic.',
+  },
+  {
+    type: 'artistic',
+    question: `I'm very sensitive. I use emotion more in my decisions.`,
+  },
+  {
+    type: 'artistic',
+    question:
+      'I am an idealistic person. I believe we can build a better world.',
+  },
+  {
+    type: 'artistic',
+    question: 'I like to create, invent, idealize and fantasize.',
+  },
+  {
+    type: 'artistic',
+    question:
+      'I like to make an impact, provoking, entertaining, seducing, enchanting people.',
+  },
+  {
+    type: 'artistic',
+    question:
+      'I like to discover new possibilities for things and situations in life.',
+  },
+  {
+    type: 'artistic',
+    question: `I often don't think long before making a decision, nor do I measure the consequences.`,
+  },
+  {
+    type: 'artistic',
+    question:
+      'I like to express myself, to represent myself or to show myself in public.',
+  },
+  {
+    type: 'artistic',
+    question: 'I am responsible for my attitudes and decisions.',
+  },
+  {
+    type: 'social',
+    question:
+      'I like to lead, organize or mobilize a group around a cause or action.',
+  },
+  {
+    type: 'social',
+    question:
+      'I like to help and collaborate with others. I am loyal and I like to do good.',
+  },
+  {
+    type: 'social',
+    question: 'I use emotion and sensitivity to make decisions.',
+  },
+  {
+    type: 'social',
+    question: 'I am generally a pleasant and easy going person.',
+  },
+  { type: 'social', question: 'I am an idealist and I want more out of life.' },
+  {
+    type: 'social',
+    question: 'Ethical, justice or common welfare issues are important to me.',
+  },
+  {
+    type: 'social',
+    question: `I'm responsible. You can count on me because I always live up to expectations.`,
+  },
+  {
+    type: 'social',
+    question: `I tend to dedicate myself to people, or causes, "wearing the shirt" of a group or club that I believe in.`,
+  },
+  {
+    type: 'social',
+    question: 'In general, I relate well with others. I find it easy to talk.',
+  },
+  {
+    type: 'social',
+    question:
+      'I like to help, teach, take care, somehow I want to be important to others.',
+  },
 ]
