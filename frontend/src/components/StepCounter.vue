c1ff57c48d33983cab498ba98ecd70db362cd5d5
Emkis <nicolasemkis@gmail.com>
2020-06-02 00:56:50 -0300

    components added

---
+++ frontend/src/components/StepCounter.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="step-counter">
+    <strong>
+      <span class="step-counter__current">{{ currentStep }}</span>
+      <span class="step-counter__separator">/</span>
+      <span class="step-counter__total">{{ totalSteps }}</span>
+    </strong>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentStep: {
+      type: Number,
+      required: true,
+    },
+    totalSteps: {
+      type: Number,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/constants/index.scss';
+
+.step-counter {
+  user-select: none;
+
+  &__current {
+    font-size: rem(22px);
+    font-weight: 700;
+    color: black;
+  }
+
+  &__separator {
+    margin: 0 4px;
+    font-weight: 500;
+    color: $color_gray;
+  }
+
+  &__total {
+    font-size: rem(15px);
+    font-weight: 500;
+    color: $color_gray;
+  }
+}
+</style>
