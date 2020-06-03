c4af0667f141a5f625f0034c0faf56a447115f46
Emkis <nicolasemkis@gmail.com>
2020-06-02 23:54:44 -0300

    loader component added

---
+++ frontend/src/components/Loader.vue
@@ -0,0 +1,29 @@
+<template>
+  <IconLoader size="30" color="#0e45fd" />
+</template>
+
+<script>
+import { IconLoader } from '@/components/icons'
+
+export default {
+  components: {
+    IconLoader,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+svg {
+  margin-right: 0 !important;
+  animation: spin 1.5s linear infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
