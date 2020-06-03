4d43ddb70524da61842ab18ff04c0a8025f6385e
Emkis <nicolasemkis@gmail.com>
2020-06-03 01:37:50 -0300

    add transitions between routes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view />
+    <transition name="slide" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 
@@ -11,3 +13,36 @@ export default {
   mixins: [fullViewportHeightMixin],
 }
 </script>
+
+<style lang="scss" scoped>
+@import '@/styles/constants/index.scss';
+
+#app::before {
+  position: fixed;
+  content: '';
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: $color_primary;
+  z-index: -1;
+}
+
+$duration: 200ms;
+$transition: ease-out;
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform $duration $transition;
+}
+
+.slide-enter {
+  opacity: 0;
+  transform: translateX(10%);
+}
+
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-10%);
+}
+</style>
